a5ad0e436e02066932193c94267923289820d04a
takatoshi.tenko <chanten811@gmail.com>
2024-03-26 23:36:16 +0900

    misc

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+ENV["RAILS_MASTER_KEY"] = "be591ac029a90841b019ec2536bae306" 
+
 # Initialize the Rails application.
 Rails.application.initialize!
